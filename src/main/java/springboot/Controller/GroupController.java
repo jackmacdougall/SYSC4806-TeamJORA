6e3b27aa506d2b8f24a596570f4acafe6db7f7f7
Jack MacDougall <jackmacdougall@cmail.carleton.ca>
2020-03-06 00:26:05 -0500

    Added page that lists all the listed groups

--- src/main/java/springboot/Controller/GroupController.java
+++ src/main/java/springboot/Controller/GroupController.java
@@ -20,11 +20,17 @@ public class GroupController {
 
     @GetMapping(value = "/addGroupPage")
     public String addGroupPage(Model model){
-        model.addAttribute("group", new Group());
+        model.addAttribute("newGroup", new Group());
         return "addGroupPage";
     }
 
-    @PostMapping(value = "/addGroup")
+    @GetMapping(value = "/groupListPage")
+    public String groupListPage(Model model){
+        model.addAttribute("groups", groupRepository.findAll());
+        return "groupListPage";
+    }
+
+    @PostMapping(value = "/group/add")
     public String addGroup(@ModelAttribute Group group) {
         groupRepository.save(group);
         return "jorahome";
