7b24b11c1f7f75dfd932c73483500b8d475431fb
OmarDawoud <omardawoud@cmail.carleton.ca>
2020-03-11 14:56:27 -0400

    Fixed Bugs in jorahome, itemPage, and ItemController
    You can now access itemPage!

--- src/main/java/springboot/Controller/ItemController.java
+++ src/main/java/springboot/Controller/ItemController.java
@@ -5,10 +5,12 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import springboot.Model.*;
 import springboot.Service.ItemService;
 
 @Controller
+@RequestMapping("/item")
 public class ItemController {
 
     private final ItemService service;
@@ -18,14 +20,15 @@ public class ItemController {
     }
 
     @GetMapping(value = "/itemPage")
-    public String itemPage() {
+    public String itemPage(Model model) {
+        model.addAttribute("item", service.getAllItems());
         return "itemPage";
     }
 
     @PostMapping(value = "/add")
     public String addItem(@ModelAttribute Item item, Model model) {
         service.addItem(item);
-        model.addAttribute("items", service.getAllItems());
+        model.addAttribute("item", service.getAllItems());
         return "itemPage";
     }
 }
