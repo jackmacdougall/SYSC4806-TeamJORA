f21b657244b1c8fdcabeb8ccea489ceb1e8678ad
OmarDawoud <omardawoud@cmail.carleton.ca>
2020-03-11 13:51:45 -0400

    Added more functionality to ItemController

--- src/main/java/springboot/Controller/ItemController.java
+++ src/main/java/springboot/Controller/ItemController.java
@@ -1,16 +1,31 @@
 package springboot.Controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import springboot.Model.*;
+import springboot.Service.ItemService;
 
 @Controller
 public class ItemController {
 
+    private final ItemService service;
+
+    private ItemController(ItemService service){
+        this.service = service;
+    }
+
     @GetMapping(value = "/itemPage")
     public String itemPage() {
+        return "itemPage";
+    }
+
+    @PostMapping(value = "/add")
+    public String addGroup(@ModelAttribute Item item, Model model) {
+        service.addItem(item);
+        model.addAttribute("items", service.getAllItems());
         return "itemPage";
     }
 }
