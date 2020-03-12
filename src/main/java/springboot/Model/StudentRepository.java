57e9ab4bc4a8f8ad30cab89b057d62ec360df5f0
Riley MacKinnon <rileymackinnon@Rileys-MacBook-Pro.local>
2020-03-12 18:38:56 -0400

    fixed the student repo to mirror the Group repo

--- src/main/java/springboot/Model/StudentRepository.java
+++ src/main/java/springboot/Model/StudentRepository.java
@@ -1,10 +1,12 @@
 package springboot.Model;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-//@RepositoryRestResource(collectionResourceRel = "students", path = "students")
-public interface StudentRepository extends CrudRepository<Student, Long>{
-   // List<Model.Student> findByName(String name);
-   // List<Model.Student> findAll();
-   // Model.Student findById(long id);
+import java.util.List;
+
+@RepositoryRestResource(collectionResourceRel = "students", path = "students")
+public interface StudentRepository extends CrudRepository<Student, Integer> {
+    Student findByName(@Param("name") String name);
 }
