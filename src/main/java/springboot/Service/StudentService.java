42b02ad74b63e406acc9544a7d05d1a318dd6dc2
Riley MacKinnon <rileymackinnon@Rileys-MacBook-Pro.local>
2020-03-12 18:26:26 -0400

    created student service

---
+++ src/main/java/springboot/Service/StudentService.java
@@ -0,0 +1,21 @@
+package springboot.Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import springboot.Model.Student;
+import springboot.Model.StudentRepository;
+
+@Service
+public class StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public Iterable<Student> getAllStudents(){
+        return studentRepository.findAll();
+    }
+
+    public void addStudent(Student student){
+        studentRepository.save(student);
+    }
+}
