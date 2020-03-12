eae51206a74fe96b13dfcabacd50dd2dc410bf86
Jack MacDougall <jackmacdougall@cmail.carleton.ca>
2020-03-11 22:18:01 -0400

    Added addGroup integration test

--- src/test/java/IntegrationTest/GroupControllerIntegrationTest.java
+++ src/test/java/IntegrationTest/GroupControllerIntegrationTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -19,11 +20,14 @@ import org.springframework.web.context.WebApplicationContext;
 import springboot.BootApplication;
 import springboot.Controller.GroupController;
 import springboot.Model.Group;
+import springboot.Model.GroupRepository;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @WebAppConfiguration
@@ -31,15 +35,20 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class GroupControllerIntegrationTest {
 
     @Autowired
-    GroupController customerController;
+    GroupController groupController;
+
+    @Autowired
+    GroupRepository groupRepository;
 
     @Autowired
     private WebApplicationContext wac;
 
     private MockMvc mockMvc;
+    private Group group;
 
     @Before
     public void setup() {
+        this.group = new Group("Integration Test Group");
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
     }
 
@@ -59,6 +68,15 @@ public class GroupControllerIntegrationTest {
                 .andExpect(view().name("groupListPage"))
                 .andExpect(model().attributeExists("groups"))
                 .andReturn();
+    }
+
+    @Test
+    public void testAddGroup() throws Exception{
+        assertNull(groupRepository.findByName(this.group.getName()));
+        this.mockMvc.perform(post("/group/add").flashAttr("group", group));
+
+        assertThat(this.group.getName()).isSameAs(groupRepository.findByName(group.getName()).getName());
+        assertThat(this.group.getId()).isSameAs(groupRepository.findByName(group.getName()).getId());
     }
 
 }
