0f2a63fdab302291d6419ce3108e228046e87aa7
andrew-vicencio <andrew.v1124@gmail.com>
2020-03-19 20:39:29 -0400

    Added rubric and item to both to allow showing in itemlistpage

--- src/main/java/springboot/Controller/ItemController.java
+++ src/main/java/springboot/Controller/ItemController.java
@@ -8,15 +8,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import springboot.Model.*;
 import springboot.Service.ItemService;
+import springboot.Service.RubricService;
 
 @Controller
 @RequestMapping("/item")
 public class ItemController {
 
     private final ItemService service;
+    private final RubricService rubricService;
 
-    private ItemController(ItemService service){
+//    private ItemController(ItemService service){
+//        this.service = service;
+//    }
+
+    private ItemController(ItemService service, RubricService rubricService){
         this.service = service;
+        this.rubricService = rubricService;
     }
 
     @GetMapping(value = "/addItemPage")
@@ -28,6 +35,7 @@ public class ItemController {
     @GetMapping(value = "/all")
     public String itemListPage(Model model) {
         model.addAttribute("item", service.getAllItems());
+        model.addAttribute("rubric", rubricService.getAllRubrics());
         return "itemListPage";
     }
 
--- src/main/java/springboot/Controller/RubricController.java
+++ src/main/java/springboot/Controller/RubricController.java
@@ -1,34 +1,51 @@
 package springboot.Controller;
 
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import springboot.Model.Item;
 import springboot.Model.Rubric;
+import springboot.Service.ItemService;
 import springboot.Service.RubricService;
 
 @Controller
 @RequestMapping("/rubric")
 public class RubricController {
     private final RubricService service;
+    private final ItemService itemService;
 
-    private RubricController(RubricService service) { this.service = service; }
+    private RubricController(RubricService service, ItemService itemService) {
+        this.service = service;
+        this.itemService = itemService;
+    }
 
-    @GetMapping(value="/add")
-    public String addRubricPage() { return "addRubricPage"; }
+    @GetMapping(value="/all")
+    public String rubricListPage(Model model) {
+        model.addAttribute("rubric", service.getAllRubrics());
+        return "rubricListPage";
+    }
 
-    @PostMapping(value="/add")
-    public String addRubric(@ModelAttribute("rubric") Rubric rubric, Model model) {
+    @GetMapping(value="/")
+    public String addRubricPage(@RequestParam Integer item, @ModelAttribute Rubric rubric, Model model) {
+        model.addAttribute("item", itemService.findById(item));
+        model.addAttribute("rubric", new Rubric());
+        return "addRubricPage";
+    }
+
+    @PostMapping(value="/")
+    public String addRubric(@ModelAttribute Rubric rubric, Model model) {
         service.addRubric(rubric);
-        model.addAttribute("rubrics", service.getAllRubrics());
+        model.addAttribute("rubric", service.getAllRubrics());
+        model.addAttribute("item", itemService.getAllItems());
         return "itemListPage";
     }
 
     @GetMapping(value = "/all")
-    public String allRubrics(Model model, Item id)
+    public String allRubrics(Model model, @RequestParam Item item) {
+        model.addAttribute("rubric", service.getAllRubricsByItem(item));
+        return "itemListPage";
+    }
 
 }
